30aafa171fd05599be29e3f35569729e36730f1b
Thang <dangcongthangcv3@gmail.com>
2023-05-14 22:03:40 +0700

    lan1

--- src/Component/ComponentForm.jsx
+++ src/Component/ComponentForm.jsx
@@ -3,7 +3,34 @@ import React, { Component } from 'react'
 export default class ComponentForm extends Component {
   render() {
     return (
-      <div>ComponentForm</div>
+      <div>
+        <h3 className='bg-dark text-white p-3'>Thông tin sinh viên</h3>
+        <div className="row">
+            <div className="col-md-6">
+                <div className="mb-3">
+                    <label for="id" >Mã SV</label>
+                    <input type="text" className="form-control" id='id'/>
+                </div>
+                <div className="mb-3">
+                    <label for="phone" >Số điện thoại</label>
+                    <input type="number" className="form-control" id='phone'/>
+                </div>  
+                <div className="mb-3">
+                    <button className='btn btn-success'>Thêm sinh viên</button>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <div className="mb-3">
+                    <label for="name" >Họ tên</label>
+                    <input type="text" className="form-control" id='name'/>
+                </div>
+                <div className="mb-3">
+                    <label for="email" >Email</label>
+                    <input type="email" className="form-control" id='email'/>
+                </div>
+            </div>
+        </div>
+      </div>
     )
   }
 }
--- src/Component/ComponentProfile.jsx
+++ src/Component/ComponentProfile.jsx
@@ -3,7 +3,18 @@ import React, { Component } from 'react'
 export default class ComponentProfile extends Component {
   render() {
     return (
-      <div>ComponentProfile</div>
+      <table className='table'>
+        <thead className='bg-dark text-white pm-3'>
+          <tr>
+            <th>Mã sinh viên</th>
+            <th>Họ tên</th>
+            <th>Số Điện Thoại</th>
+            <th>Email</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     )
   }
 }
