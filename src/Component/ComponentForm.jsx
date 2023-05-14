5dd195be8917facc90f64bdc1643454c13cb4cb7
Thang <dangcongthangcv3@gmail.com>
2023-05-14 23:50:50 +0700

    lan1

--- src/Component/ComponentForm.jsx
+++ src/Component/ComponentForm.jsx
@@ -1,36 +1,78 @@
 import React, { Component } from 'react'
 
 export default class ComponentForm extends Component {
+  state = {
+    values:{
+      idSV:'',
+      phone:'',
+      name:'',
+      email:''
+    },
+    errors:{
+      idSV:'',
+      phone:'',
+      name:'',
+      email:''
+    }
+  }
+  handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(this.state.values)
+  }
+  handleChangeInput = (e) =>{
+    let {value,id} = e.target;
+
+    let newValue = {...this.state.values}
+    newValue[id] = value
+
+    let newError = {...this.state.errors}
+    let messError = '';
+    if(value.trim()===""){
+      messError = id + ' không được bỏ trống';
+    }
+    newError[id] = messError
+
+    this.setState({
+      values: newValue,
+      errors: newError
+    },()=>{
+      console.log(this.state)
+    })
+  }
   render() {
     return (
-      <div>
+      <form onSubmit={this.handleSubmit}>
         <h3 className='bg-dark text-white p-3'>Thông tin sinh viên</h3>
         <div className="row">
             <div className="col-md-6">
                 <div className="mb-3">
-                    <label for="id" >Mã SV</label>
-                    <input type="text" className="form-control" id='id'/>
+                    <label htmlFor="idSV" >Mã SV</label>
+                    <input data-type="text" className="form-control" id='idSV' name='idSV' onInput={this.handleChangeInput}/>
+                    <p className='text text-danger'>{this.state.errors.idSV}</p>
                 </div>
                 <div className="mb-3">
-                    <label for="phone" >Số điện thoại</label>
-                    <input type="number" className="form-control" id='phone'/>
+                    <label htmlFor="phone" >Số điện thoại</label>
+                    <input data-minLength={9} data-maxLength={12} className="form-control" id='phone' name='phone' onInput={this.handleChangeInput}/>
+                    <p className='text text-danger'>{this.state.errors.phone}</p>
                 </div>  
                 <div className="mb-3">
-                    <button className='btn btn-success'>Thêm sinh viên</button>
+                    <button type='submit' className='btn btn-success'>Thêm sinh viên</button>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
-                    <label for="name" >Họ tên</label>
-                    <input type="text" className="form-control" id='name'/>
+                    <label htmlFor="name" >Họ tên</label>
+                    <input data-type="text" className="form-control" id='name' name='name' onInput={this.handleChangeInput}/>
+                    <p className='text text-danger'>{this.state.errors.name}</p>
                 </div>
                 <div className="mb-3">
-                    <label for="email" >Email</label>
-                    <input type="email" className="form-control" id='email'/>
+                    <label htmlFor="email" >Email</label>
+                    <input data-type="email" className="form-control" id='email' name='email' onInput={this.handleChangeInput}/>
+                    <p className='text text-danger'>{this.state.errors.email}</p>
                 </div>
             </div>
         </div>
-      </div>
+      </form>
     )
   }
 }
