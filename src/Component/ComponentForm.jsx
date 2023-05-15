17daa2ed76bb1c9ff070fd8ec7f49c088312f95b
Thang <dangcongthangcv3@gmail.com>
2023-05-16 03:16:32 +0700

    lan1

--- src/Component/ComponentForm.jsx
+++ src/Component/ComponentForm.jsx
@@ -9,15 +9,24 @@ export default class ComponentForm extends Component {
       email:''
     },
     errors:{
-      idSV:'',
-      phone:'',
-      name:'',
-      email:''
+      idSV:'(*)',
+      phone:'(*)',
+      name:'(*)',
+      email:'(*)'
     }
   }
   handleSubmit = (e) =>{
     e.preventDefault()
-    console.log(this.state.values)
+    for (let key in this.state.errors){
+      if(this.state.errors[key] !== ''){
+        alert('Dữ liệu chưa hợp lệ')
+        return;
+      }
+    }
+
+    let {addProduct} = this.props
+    //Nếu kiểm tra this.state.values hợp lệ thì mới cho addProduct
+    addProduct(this.state.values)
   }
   handleChangeInput = (e) =>{
     let {value,id} = e.target;
@@ -33,16 +42,16 @@ export default class ComponentForm extends Component {
     }else{
       if(dataType){
         switch(dataType){
-          case 'idsv': {
-            let regexNumber = /d$/
+          case 'ma': {
+            let regexNumber = /[0-9]$/
             if(!regexNumber.test(value)){
-              messError = "Mã sv: SV01";
+              messError = "Mã sv: 1";
             };break
           }
           case 'name': {
-            let regexNumber = /^[a-z A-Z]+$/
+            let regexNumber = /[a-z A-Z]$/
             if(!regexNumber.test(value)){
-              messError = 'họ tên: Nguyễn Văn A';
+              messError = 'họ tên: Nguyen Van A';
             };break
           }
           case 'phone': {
@@ -69,35 +78,47 @@ export default class ComponentForm extends Component {
       console.log(this.state)
     })
   }
+
+  componentWillReceiveProps(newProps){
+    this.setState({
+      values:newProps.productEdit
+    })
+  }
   render() {
+    let {idSV,name,phone,email} = this.state.values
     return (
+
       <form onSubmit={this.handleSubmit}>
         <h3 className='bg-dark text-white p-3'>Thông tin sinh viên</h3>
         <div className="row">
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="idSV" >Mã SV</label>
-                    <input data-type="idsv" className="form-control" id='idSV' name='idSV' onInput={this.handleChangeInput}/>
+                    <input data-type="ma" value={idSV} className="form-control" id='idSV' name='idSV' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.idSV}</p>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" >Số điện thoại</label>
-                    <input data-type='phone' className="form-control" id='phone' name='phone' onInput={this.handleChangeInput}/>
+                    <input data-type='phone' value={phone} className="form-control" id='phone' name='phone' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.phone}</p>
                 </div>  
                 <div className="mb-3">
                     <button type='submit' className='btn btn-success'>Thêm sinh viên</button>
+                    <button type='button' className='btn btn-success' onClick={()=>{
+                      let {updateProduct} = this.props
+                      updateProduct({...this.state.values})
+                    }}>Cập nhập sinh viên</button>
                 </div>
             </div>
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="name" >Họ tên</label>
-                    <input data-type="name" className="form-control" id='name' name='name' onInput={this.handleChangeInput}/>
+                    <input data-type="name" value={name} className="form-control" id='name' name='name' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.name}</p>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="email" >Email</label>
-                    <input data-type="email" className="form-control" id='email' name='email' onInput={this.handleChangeInput}/>
+                    <input data-type="email" value={email} className="form-control" id='email' name='email' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.email}</p>
                 </div>
             </div>
--- src/Component/ComponentProfile.jsx
+++ src/Component/ComponentProfile.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 
 export default class ComponentProfile extends Component {
+  renderProfile = ()=>{
+    let {arrProduct,delProduct,editProduct} = this.props
+    return arrProduct.map((prod)=>{
+      return <tr key={prod.idSV}>
+        <td>{prod.idSV}</td>
+        <td>{prod.name}</td>
+        <td>{prod.phone}</td>
+        <td>{prod.email}</td>
+        <td><button className='btn btn-danger' onClick={()=>{
+          delProduct(prod.idSV)
+        }}>Xóa</button>
+        <button className='btn btn-primary' onClick={()=>{
+          editProduct(prod)
+        }}>Sửa</button></td>
+      </tr>
+    })
+  }
   render() {
     return (
       <table className='table'>
@@ -13,7 +30,9 @@ export default class ComponentProfile extends Component {
             <th></th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {this.renderProfile()}
+        </tbody>
       </table>
     )
   }
