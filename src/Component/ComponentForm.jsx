b1a9573c269e0dc1979bcc84cd027d1f423be132
Thang <dangcongthangcv3@gmail.com>
2023-05-16 00:38:47 +0700

    lan1

--- src/Component/ComponentForm.jsx
+++ src/Component/ComponentForm.jsx
@@ -21,6 +21,7 @@ export default class ComponentForm extends Component {
   }
   handleChangeInput = (e) =>{
     let {value,id} = e.target;
+    let dataType =e.target.getAttribute('data-type')
 
     let newValue = {...this.state.values}
     newValue[id] = value
@@ -29,6 +30,35 @@ export default class ComponentForm extends Component {
     let messError = '';
     if(value.trim()===""){
       messError = id + ' không được bỏ trống';
+    }else{
+      if(dataType){
+        switch(dataType){
+          case 'idsv': {
+            let regexNumber = /d$/
+            if(!regexNumber.test(value)){
+              messError = "Mã sv: SV01";
+            };break
+          }
+          case 'name': {
+            let regexNumber = /^[a-z A-Z]+$/
+            if(!regexNumber.test(value)){
+              messError = 'họ tên: Nguyễn Văn A';
+            };break
+          }
+          case 'phone': {
+            let regexNumber = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/
+            if(!regexNumber.test(value)){
+              messError = ' số điện thoại: 0909090909 ';
+            };break
+          }
+          case 'email': {
+            let regexNumber = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+            if(!regexNumber.test(value)){
+              messError = ' email: test@gmail.com';
+            };break
+          }
+        }
+      }
     }
     newError[id] = messError
 
@@ -47,12 +77,12 @@ export default class ComponentForm extends Component {
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="idSV" >Mã SV</label>
-                    <input data-type="text" className="form-control" id='idSV' name='idSV' onInput={this.handleChangeInput}/>
+                    <input data-type="idsv" className="form-control" id='idSV' name='idSV' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.idSV}</p>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="phone" >Số điện thoại</label>
-                    <input data-minLength={9} data-maxLength={12} className="form-control" id='phone' name='phone' onInput={this.handleChangeInput}/>
+                    <input data-type='phone' className="form-control" id='phone' name='phone' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.phone}</p>
                 </div>  
                 <div className="mb-3">
@@ -62,7 +92,7 @@ export default class ComponentForm extends Component {
             <div className="col-md-6">
                 <div className="mb-3">
                     <label htmlFor="name" >Họ tên</label>
-                    <input data-type="text" className="form-control" id='name' name='name' onInput={this.handleChangeInput}/>
+                    <input data-type="name" className="form-control" id='name' name='name' onInput={this.handleChangeInput}/>
                     <p className='text text-danger'>{this.state.errors.name}</p>
                 </div>
                 <div className="mb-3">
